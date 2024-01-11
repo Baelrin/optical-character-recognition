a477a729782f9d144feba5f2a0afa459edf2a1b7
Baelrin <Baelrin-Revel@yandex.ru>
2024-01-11 16:24:17 +0300

    Create basic structure

--- master.py
+++ master.py
@@ -1,1 +1,10 @@
+import easyocr
 
+
+def is_text_present(image_path):
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(image_path)
+    return len(result) > 0
+
+
+print(is_text_present('images/1.png'))
